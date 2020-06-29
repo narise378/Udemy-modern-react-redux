b17c3008cf2d3aa0811613db60b673dcbfeea78c
narise378 <narise378@gmail.com>
2020-06-29 21:31:50 +0900

    setup index.js React,ReactDOM

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Import the React and ReactDOM libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Create a react Component
+const App = () => {
+  return <div>Hi React!!</div>
+}
+// Take the react Component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'))
