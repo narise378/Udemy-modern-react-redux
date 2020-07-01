e5e1a21d47b852d31fa42965dd1151567818dd4c
narise378 <narise378@gmail.com>
2020-07-01 10:54:30 +0900

    Building content with JSX Learn

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// function getButtonText() {
+//   return 'Click on Me!'
+// }
+
 // Create a react Component
 const App = () => {
-  return <div>Hi React!!</div>
+  const buttonText = { text: 'Click Me!' }
+  const labelText = 'Enter Name : '
+  const style = { backgroundColor: 'blue', color: 'white' }
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>{buttonText.text}</button>
+    </div>
+  )
 }
 // Take the react Component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'))
