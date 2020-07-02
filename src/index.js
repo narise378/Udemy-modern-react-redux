f163168d067bbd500eeb6e0947eed2f6ba33f158
narise378 <narise378@gmail.com>
2020-07-02 13:56:40 +0900

    Learn class App Component

--- src/index.js
+++ src/index.js
@@ -1,46 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
-import CommentDetail from './CommentDetail'
-import ApprovalCard from './ApprovalCard'
 
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <div>
-          <h4>Waring!!</h4>
-          Aer you sure you want to do this?
-        </div>
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          avatar={faker.image.avatar()}
-          author="Sam"
-          timeAgo="Today at 4:45PM"
-          content="Nice blog post!!"
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          avatar={faker.image.avatar()}
-          author="Alex"
-          timeAgo="Today at 2:45PM"
-          content="I Like this!"
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          avatar={faker.image.avatar()}
-          author="Jane"
-          timeAgo="Yesterday at 4:45PM"
-          content="Bad blog post!!"
-        />
-      </ApprovalCard>
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    )
+    return <div>Latitude: </div>
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
