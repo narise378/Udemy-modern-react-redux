e8148295fb4b2ec9cd52888ee4d255ce5b08a7b1
narise378 <narise378@gmail.com>
2020-07-03 06:53:33 +0900

    Learn State in React use Giolocation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props)
+
+    // THIS IS THE ONRY TIME we do direct assignment
+    this.state = { lat: null, errorMessage: '' }
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        // We called setstate!!
+        this.setState({ lat: position.coords.latitude })
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
     )
-    return <div>Latitude: </div>
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Eroor: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loding...</div>
   }
 }
 
