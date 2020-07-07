b4609dbb6774c337bb09e71da3fd41726a234610
narise378 <narise378@gmail.com>
2020-07-08 01:30:54 +0900

    using refs implement unsplash api app

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ImageCard from './ImageCard'
+import './ImageList.css'
 
 const ImageList = (props) => {
   // console.log(props.images)
@@ -7,10 +9,10 @@ const ImageList = (props) => {
   //     <img key={image.id} src={image.urls.regular} alt={image.description} />
   //   )
   // })
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img key={id} src={urls.regular} alt={description} />
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />
   })
-  return <div>{images}</div>
+  return <div className="image-list">{images}</div>
 }
 
 export default ImageList
