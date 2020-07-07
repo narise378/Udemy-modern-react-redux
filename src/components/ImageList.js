ac5112502366376484b44994202c4f8f66b2e44e
narise378 <narise378@gmail.com>
2020-07-07 19:09:41 +0900

    building lists of records implement

---
+++ src/components/ImageList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ImageList = (props) => {
+  // console.log(props.images)
+  // const images = props.images.map((image) => {
+  //   return (
+  //     <img key={image.id} src={image.urls.regular} alt={image.description} />
+  //   )
+  // })
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img key={id} src={urls.regular} alt={description} />
+  })
+  return <div>{images}</div>
+}
+
+export default ImageList
