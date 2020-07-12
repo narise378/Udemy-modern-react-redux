f39bfaebf67f33436a2bbe682d98ec1bc08f05be
narise378 <narise378@gmail.com>
2020-07-12 17:41:20 +0900

    deploy for Netlify

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form onSubmit={onSubmit} className="ui form">
         <div className="field">
-          <label>Video Search</label>
+          <label>Video Search for Youtube...</label>
           <input
             type="text"
             value={term}
