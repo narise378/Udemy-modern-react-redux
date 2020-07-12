ab466cdf4061b916db862643a144ebf7579b0e17
narise378 <narise378@gmail.com>
2020-07-12 11:54:17 +0900

    Refactoring Youtube App on React Hooks or Learn Custom Hooks

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -4,11 +4,7 @@ import VideoItem from './VideoItem'
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderList = videos.map((video) => {
     return (
-      <VideoItem
-        key={video.id.videoId}
-        onVideoSelect={onVideoSelect}
-        video={video}
-      />
+      <VideoItem key={video.etag} onVideoSelect={onVideoSelect} video={video} />
     )
   })
   return <div className="ui relaxed divided list">{renderList}</div>
